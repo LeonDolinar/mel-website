26f1958ade0b509346b0b7f5f319dd91167ec175
Leon <73344621+LeonDolinar@users.noreply.github.com>
2024-10-25 19:47:53 +0900

    Fixed kontakt (#6)

--- process.php
+++ process.php
@@ -1,54 +1,49 @@
 <?php
-// Configure your Subject Prefix and Recipient here
-$subjectPrefix = '[Contact via website]';
-$emailTo       = '<YOUR_EMAIL_HERE>';
-$errors = array(); // array to hold validation errors
-$data   = array(); // array to pass back data
+$errors = [];
+$data   = [];
+$emailTo = 'mel@mel-doo.com';
+
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name    = stripslashes(trim($_POST['name']));
-    $email   = stripslashes(trim($_POST['email']));
-    $subject = stripslashes(trim($_POST['subject']));
-    $message = stripslashes(trim($_POST['message']));
+    $name = htmlspecialchars(trim($_POST['name']));
+    $email = htmlspecialchars(trim($_POST['email']));
+    $phone = htmlspecialchars(trim($_POST['phone']));
+    $message = htmlspecialchars(trim($_POST['message']));
+
     if (empty($name)) {
-        $errors['name'] = 'Name is required.';
+        $errors['name'] = 'Molimo unesite svoje ime';
     }
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors['email'] = 'Email is invalid.';
-    }
-    if (empty($subject)) {
-        $errors['subject'] = 'Subject is required.';
+        $errors['email'] = 'Molimo unesite svoju email adresu';
     }
     if (empty($message)) {
-        $errors['message'] = 'Message is required.';
+        $errors['message'] = 'Molimo unesite poruku';
     }
-    // if there are any errors in our errors array, return a success boolean or false
+    
     if (!empty($errors)) {
         $data['success'] = false;
         $data['errors']  = $errors;
+        http_response_code(400);
     } else {
-        $subject = "$subjectPrefix $subject";
-        $body    = '
-            <strong>Name: </strong>'.$name.'<br />
-            <strong>Email: </strong>'.$email.'<br />
-            <strong>Message: </strong>'.nl2br($message).'<br />
-        ';
-        $headers  = "MIME-Version: 1.1" . PHP_EOL;
+        $body = "
+            <strong>Ime: </strong>$name<br />
+            <strong>Email: </strong>$email<br />
+            <strong>Telefon: </strong>$phone<br />
+            <strong>Poruka: </strong>" . nl2br($message) . "<br />
+        ";
+        $headers = "MIME-Version: 1.1" . PHP_EOL;
         $headers .= "Content-type: text/html; charset=utf-8" . PHP_EOL;
-        $headers .= "Content-Transfer-Encoding: 8bit" . PHP_EOL;
-        $headers .= "Date: " . date('r', $_SERVER['REQUEST_TIME']) . PHP_EOL;
-        $headers .= "Message-ID: <" . $_SERVER['REQUEST_TIME'] . md5($_SERVER['REQUEST_TIME']) . '@' . $_SERVER['SERVER_NAME'] . '>' . PHP_EOL;
-        $headers .= "From: " . "=?UTF-8?B?".base64_encode($name)."?=" . "<$email>" . PHP_EOL;
-        $headers .= "Return-Path: $emailTo" . PHP_EOL;
+        $headers .= "From: MEL d.o.o. <$emailTo>" . PHP_EOL;
         $headers .= "Reply-To: $email" . PHP_EOL;
-        $headers .= "X-Mailer: PHP/". phpversion() . PHP_EOL;
-        $headers .= "X-Originating-IP: " . $_SERVER['SERVER_ADDR'] . PHP_EOL;
-        mail($emailTo, "=?utf-8?B?" . base64_encode($subject) . "?=", $body, $headers);
-        $data['success'] = true;
-        $data['message'] = 'Congratulations. Your message has been sent successfully';
+        $headers .= "X-Mailer: PHP/" . phpversion() . PHP_EOL;
+
+        if (mail($emailTo, 'Upit putem web stranice', $body, $headers)) {
+            $data['success'] = true;
+            $data['message'] = 'Vaša poruka je uspješno poslana';
+        } else {
+            $data['success'] = false;
+            $data['message'] = 'Došlo je do pogreške prilikom slanja vaše poruke, pokušajte ponovno';
+            http_response_code(500);
+        }
     }
-    // return all our data to an AJAX call
     echo json_encode($data);
 }
-
-
-
